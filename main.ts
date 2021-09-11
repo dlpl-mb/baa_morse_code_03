e91bb49c1ec34058b615495d81e29fcc13303ea2
BAA2 <alois.bachinger@ph-linz.at>
2021-09-11 22:45:03 +0200

    main:

--- main.ts
+++ main.ts
@@ -1,73 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    if (ich_bin_sender == 1) {
-        radio.sendString("" + (auswahl_morsecode))
-    } else {
-        radio.sendString("ok")
-    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    zufall = randint(0, 25)
+    basic.showString("" + (liste_buchstaben[zufall]))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("" + (liste_morsecodes[zufall]))
 })
-input.onButtonPressed(Button.AB, function () {
-    ich_bin_sender = 1
-})
-radio.onReceivedString(function (receivedString) {
-    basic.clearScreen()
-    basic.showString(receivedString)
-    basic.pause(500)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("no")
-    ich_bin_sender = 0
-})
-let auswahl_buchstabe = ""
-let index = 0
-let neigung = 0
-let auswahl_morsecode = ""
-let ich_bin_sender = 0
-basic.showLeds(`
-    . . # . .
-    # # # # #
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-let liste_morsecodes = [
-".-",
-"-...",
-"-.-.",
-"-..",
-".",
-"..-.",
-"--.",
-"....",
-"..",
-".---",
-"-.-",
-".-..",
-"--",
-"-.",
-"---",
-".--.",
-"--.-",
-".-.",
-"...",
-"-",
-"..-",
-"...-",
-".--",
-"-..-",
-"-.--",
-"--..",
-".----",
-"..---",
-"...--",
-"....-",
-".....",
-"-....",
-"--...",
-"---..",
-"----.",
-"-----"
-]
-let liste_buchstaben = [
+let zufall = 0
+let liste_morsecodes: string[] = []
+let liste_buchstaben: string[] = []
+basic.showIcon(IconNames.Yes)
+liste_buchstaben = [
 "A",
 "B",
 "C",
@@ -93,40 +47,33 @@ let liste_buchstaben = [
 "W",
 "X",
 "Y",
-"Z",
-"1",
-"2",
-"3",
-"4",
-"5",
-"6",
-"7",
-"8",
-"9",
-"0"
+"Z"
 ]
-music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-let anz_bst = liste_buchstaben.length - 1
-radio.setGroup(99)
-ich_bin_sender = 0
-basic.forever(function () {
-    if (ich_bin_sender == 1) {
-        neigung = input.acceleration(Dimension.X)
-        if (neigung > 300) {
-            index += 1
-        }
-        if (neigung < -300) {
-            index += -1
-        }
-        if (index > anz_bst) {
-            index = 0
-        }
-        if (index < 0) {
-            index = anz_bst
-        }
-        auswahl_buchstabe = liste_buchstaben[index]
-        auswahl_morsecode = liste_morsecodes[index]
-        basic.showString("" + (auswahl_buchstabe))
-        basic.pause(300)
-    }
-})
+liste_morsecodes = [
+".-",
+"-...",
+"-.-.",
+"-..",
+".",
+"..-.",
+"--.",
+"....",
+"..",
+".---",
+"-.-",
+".-..",
+"--",
+"-.",
+"---",
+".--.",
+"--.-",
+".-.",
+"...",
+"-",
+"..-",
+"...-",
+".--",
+"-..-",
+"-.--",
+"--.."
+]
