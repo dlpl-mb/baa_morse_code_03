1e5ba968eacf2bcba1711fbae6d330afd90c9f9b
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-09-27 00:08:16 +0200

    Aktualisiere pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,55 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    zufall = randint(0, 25)
-    basic.showString("" + (liste_buchstaben[zufall]))
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showString("" + (liste_morsecodes[zufall]))
+    if (ich_bin_sender == 1) {
+        radio.sendString("" + (auswahl_morsecode))
+    } else {
+        radio.sendString("+")
+    }
 })
-let zufall = 0
-let liste_morsecodes: string[] = []
-let liste_buchstaben: string[] = []
-basic.showIcon(IconNames.Yes)
-liste_buchstaben = [
-"A",
-"B",
-"C",
-"D",
-"E",
-"F",
-"G",
-"H",
-"I",
-"J",
-"K",
-"L",
-"M",
-"N",
-"O",
-"P",
-"Q",
-"R",
-"S",
-"T",
-"U",
-"V",
-"W",
-"X",
-"Y",
-"Z"
-]
-liste_morsecodes = [
+input.onButtonPressed(Button.AB, function () {
+    ich_bin_sender = 1
+})
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("-")
+    ich_bin_sender = 0
+})
+let auswahl_buchstabe = ""
+let index = 0
+let neigung = 0
+let auswahl_morsecode = ""
+let ich_bin_sender = 0
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+let liste_morsecodes = [
 ".-",
 "-...",
 "-.-.",
@@ -75,5 +55,78 @@ liste_morsecodes = [
 ".--",
 "-..-",
 "-.--",
-"--.."
+"--..",
+".----",
+"..---",
+"...--",
+"....-",
+".....",
+"-....",
+"--...",
+"---..",
+"----.",
+"-----"
 ]
+let liste_buchstaben = [
+"A",
+"B",
+"C",
+"D",
+"E",
+"F",
+"G",
+"H",
+"I",
+"J",
+"K",
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z",
+"1",
+"2",
+"3",
+"4",
+"5",
+"6",
+"7",
+"8",
+"9",
+"0"
+]
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+let anz_bst = liste_buchstaben.length - 1
+radio.setGroup(99)
+ich_bin_sender = 0
+basic.forever(function () {
+    if (ich_bin_sender == 1) {
+        neigung = input.acceleration(Dimension.X)
+        if (neigung > 250) {
+            index += 1
+        }
+        if (neigung < -250) {
+            index += -1
+        }
+        if (index > anz_bst) {
+            index = 0
+        }
+        if (index < 0) {
+            index = anz_bst
+        }
+        auswahl_buchstabe = liste_buchstaben[index]
+        auswahl_morsecode = liste_morsecodes[index]
+        basic.showString("" + (auswahl_buchstabe))
+        basic.pause(300)
+    }
+})
